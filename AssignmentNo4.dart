efeccabc021fbf13035aa187d628c6112bee981b
Shehlaali <94216278+Shehlaali@users.noreply.github.com>
2021-12-03 18:42:51 +0500

    Add files via upload

---
+++ AssignmentNo4.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+void main()
+{
+      
+      
+// QUESTION N0: 01
+
+// List stringList = ["shehla", "Ali" ,"khaskheli"  ];
+// print(stringList);
+
+
+// QUESTION NO: 02
+
+      // List mixedList = ["shehla", 22 , true , 3.14  ];
+      // print(mixedList);
+
+
+//QUESTION NO: 03
+
+       // List education = ["SSC" , "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.Phil." , "PhD"];
+       // print(education);
+
+
+//QUESTION NO: 04
+
+            // List stdNames = ["madiha", "maliha", "alaya"];
+            // List stdScore = [400, 334, 282];
+
+            // print("Student Name : ${stdNames[0]}stdScore : ${stdScore[0]} per :${stdScore[0]/500*100}");
+
+            // print("studentName : ${stdNames[1]}stdScore : ${stdScore[1]} per :${stdScore[1]/500*100}");
+
+            // print("stdudentName : ${stdNames[2]}stdScore : ${stdScore[2]} per :${stdScore[2]/500*100}");
+
+
+ // QUESTION NO: 05           
+          
+                     //  List assendingOrder =[80, 60, 74, 32, 49, 70, 51];      
+                     //  assendingOrder.sort();
+                     //  print(assendingOrder);
+
+
+ // QUESTION NO: 06                    
+             
+                               // List desendingOrder =[80, 60, 74, 32, 49, 70, 51];      
+                               // desendingOrder.sort();
+                               // var d =List.of(desendingOrder.reversed);     
+                               // print(d);
+
+
+// QUESTION NO: 07
+
+                // List language = ["C++", "python", "dart", "javaScript"];
+
+                // if (language[1]=="")
+                //   {
+                      
+                //        print("not Present");
+
+                //   }
+
+                //      else
+                //       {
+
+                //           language.replaceRange(2,3,["ZABTech","Faith"]);
+                        
+   
+                //       }
+
+                //           print(language);
+
+
+}
+
+
